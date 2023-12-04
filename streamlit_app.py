3f2f3a60557809309cbc88bf7324a305a3d39772
rramsarup1 <150535473+rramsarup1@users.noreply.github.com>
2023-12-04 20:04:39 +0200

    Update streamlit_app.py
    added stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("breakfast menu")
+
+streamlit.text('Option 1')
+streamlit.text('Option 2')
+streamlit.text('Option 3')
