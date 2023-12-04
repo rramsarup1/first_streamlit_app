2dea705a80ed914c571d2298089e457815984cff
rramsarup1 <150535473+rramsarup1@users.noreply.github.com>
2023-12-04 20:06:57 +0200

    Update streamlit_app.py
    edhgd

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("breakfast menu")
 
-streamlit.text('Option 1')
-streamlit.text('Option 2')
-streamlit.text('Option 3')
+streamlit.text('🍞Option 1')
+streamlit.text('🥑Option 2')
+streamlit.text('🥗 Option 3')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
